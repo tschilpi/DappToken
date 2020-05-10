6e35ad8d1d2f22af950c346098f6b16259dc5739
tschilpi <marios.vettas@bluewin.ch>
2020-05-10 15:40:10 -0400

    2. Transfer Tokens

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -1,13 +1,43 @@
 pragma solidity ^0.6.7;
 
 contract DappToken {
-	// Constructor
-	// Set the total number of tokens
-	// Read the total number of tokens
+	// Name
+	string public name = "DApp Token";
+	// Symbol
+	string public symbol = "DAPP";
+	string public standard = "DApp Token v.1.0";
 	uint256 public totalSupply;
 
-	constructor () public {
-		totalSupply = 1000000;
+	event Transfer(
+		address indexed _from,
+		address indexed _to,
+		uint256 _value
+	);
+
+	mapping(address => uint256) public balanceOf;
+
+	constructor (uint256 _initialSupply) public {
+		balanceOf[msg.sender] = _initialSupply;
+		totalSupply = _initialSupply;
+		// allocate the initial supply
+	}
+
+	// Transfer
+	function transfer(address _to, uint256 _value) public returns (bool success) {
+		// Exception if account doesn't have enough
+		require(balanceOf[msg.sender] >= _value);
+
+		balanceOf[msg.sender] -= _value;
+		balanceOf[_to] += _value;
+
+		// Transfer Event
+		emit Transfer(msg.sender, _to, _value);
+
+		// Return a boolean
+		return true; 
+		
+		
+		
 	}
 }
 
